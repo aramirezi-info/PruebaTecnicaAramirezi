6e63b9af969b502da74e4fafb3ca44afd71b3987
Alejandro Ramirez <aramirezi.info@gmail.com>
2020-11-28 10:33:03 -0500

    fix: test load home page

--- tests/Feature/HomeTest.php
+++ tests/Feature/HomeTest.php
@@ -2,12 +2,16 @@
 
 namespace Tests\Feature;
 
+use App\Models\Product;
+
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class HomeTest extends TestCase
 {
+    use RefreshDatabase; 
+
     /** @test */
     public function it_visit_page_of_home()
     {
@@ -15,5 +19,21 @@ class HomeTest extends TestCase
         $response = $this->get('/');
 
         $response->assertStatus(200);
+    }
+
+    /** @test */
+    public function it_load_products_of_index()
+    {
+        $this->withoutExceptionHandling();
+
+        factory(Product::class,6)->create();
+    
+        $response = $this->get('/');
+        $response->assertOk();
+
+        $products = Product::all();
+
+        $response->assertViewIs('welcome');
+        $response->assertViewHas('products',$products);
     }
 }
